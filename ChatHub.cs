55d38c683364a86d9120e2feff5e1628a2973813
ntacsharp <92778429+ntacsharp@users.noreply.github.com>
2022-08-19 09:34:13 +0700

    Add files via upload

---
+++ ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using ChatService;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat_App.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string botUser;
+
+        public ChatHub()
+        {
+            botUser = "myBot";
+        }
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+
+            await Clients.Groups(userConnection.Room).SendAsync("ReceiveMessage", botUser, $"{userConnection.User} has joined {userConnection.Room}");
+        }
+    }
+}
