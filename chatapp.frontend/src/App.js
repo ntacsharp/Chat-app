493513f4792017ebbe05623463db4175011c35a7
ntacsharp <92778429+ntacsharp@users.noreply.github.com>
2022-08-19 10:45:45 +0700

    Add files via upload

--- chatapp.frontend/src/App.js
+++ chatapp.frontend/src/App.js
@@ -4,10 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Lobby from './components/Lobby';
 import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
 import { useState } from 'react';
+import Chat from './components/Chat';
 
 const App = () => {
   const [connection, setConnection] = useState();
-  const [message, setMessages] = useState([]);
+  const [messages, setMessages] = useState([]);
 
   const joinRoom = async (user, room) => {
     try {
@@ -17,22 +18,25 @@ const App = () => {
       .build();
 
       connection.on("ReceiveMessage", (user, message) => {
-        setMessages(message => [...message, {user, message}]);
+        setMessages(messages => [...messages, { user, message }]);
       });
 
       await connection.start();
-      await connection.invoke("JoinRoom", {user, room});
+      await connection.invoke("JoinRoom", { user, room });
       setConnection(connection);
-    } catch(e) {
+    } catch (e) {
       console.log(e);
     }
   }
 
   return <div className='app'>
     <h2>MyChat</h2>
-    <hr className= 'line' />
-    <Lobby joinRoom = {joinRoom}/>
+    <hr className='line' />
+    {!connection
+      ? <Lobby joinRoom={joinRoom} />
+      : <Chat messages={messages}/>}
   </div>
+
 }
 
 export default App;
